8be5c256f0497ad37497f7663eaeddd089ec9a14
DavidElone <david-elone@campus.technion.ac.il>
2022-09-16 07:47:32 -0400

    end of friday

--- contracts/HealthCare.sol
+++ contracts/HealthCare.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+// import "./PriceConverter.sol";
+// import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract HealthCare is VRFConsumerBase, Ownable {
-    using PriceConverter for uint256;
+contract HealthCare is Ownable {
+        // using PriceConverter for uint256;
 
     enum CONTRACT_TYPE {
         SILVER, //50% of the bill
@@ -19,51 +20,93 @@ contract HealthCare is VRFConsumerBase, Ownable {
     uint256[] public clientsID;
     address payable[] public players;
     address payable public Winner;
-
+  
     struct client {
-        address accountAdress;
+        address payable accountAddress;
         CONTRACT_TYPE contractType;
         uint256 expirationDate;
         bool inLottery;
     }
 
     mapping(uint256 => client) public clients; //map id to client
-    mapping(address => bool) public doctormapping;
+    mapping(address => bool) public doctors;
 
-    function storeclientData(
+    function addNewClient(
         uint256 id,
         CONTRACT_TYPE cntType,
-        address accountAdr,
-        uint256 expDate
+        address payable accountAddr
     ) public {
-        require(clients[id] == 0);
+        require(clients[id].accountAddress == address(0));
         clients[id].contractType = cntType;
-        clients[id].accountAdress = accountAdr;
-        clients[id].expirationDate = expDate;
-        clients[id].inLottery = true;
+        clients[id].accountAddress = accountAddr;
+        clients[id].expirationDate = block.timestamp + 365 days;
+        clientsID.push(id);
+    }
+    function extendClient(
+        uint256 id,
+        CONTRACT_TYPE cntType,
+        address payable accountAddr
+    ) public {
+        require(clients[id].accountAddress == msg.sender && clients[id].contractType == cntType);
+        if (block.timestamp < clients[id].expirationDate ){
+            clients[id].expirationDate += 365 days;
+        }else{
+            clients[id].expirationDate += block.timestamp + 365 days;
+        }
+        
+       
     }
 
-    function setDoctor(address _address) public onlySmartContractOwner {
-        require(!doctormapping[_address]);
-        doctormapping[_address] = true;
+
+    function setDoctor(address _address) public onlyOwner {
+        require(!doctors[_address]);
+        doctors[_address] = true;
     }
 
-    function subscribe(uint256 idEncrypted) public payable {
+    function subscribe(uint256 idHashed) public payable {
         CONTRACT_TYPE contractType;
-        if (PriceConverter.getConversionRate(msg.value) == SilverPrice * 12)
+        if ((msg.value) == SilverPrice )
             contractType = CONTRACT_TYPE.SILVER;
-        else if (PriceConverter.getConversionRate(msg.value) == GoldPrice * 12)
+        else if ((msg.value) == GoldPrice)
             contractType = CONTRACT_TYPE.GOLD;
-        else if (
-            PriceConverter.getConversionRate(msg.value) == PlatiniumPrice * 12
-        ) contractType = CONTRACT_TYPE.PLATINIUM;
+        else if ((msg.value) == PlatiniumPrice) 
+            contractType = CONTRACT_TYPE.PLATINIUM;
         else revert();
-
-        storeclientData(
-            idEncrypted,
-            contractType,
-            msg.sender,
-            block.timestamp + 365 days
-        );
+        if(clients[idHashed].accountAddress == address(0)){
+            addNewClient(
+                idHashed,
+                contractType,
+                payable(msg.sender)
+            );
+        }else if(clients[idHashed].accountAddress == (msg.sender)){
+            extendClient(
+                idHashed,
+                contractType,
+                payable(msg.sender)
+            );
+        }
     }
+    function refundClient(uint256 idHashed , uint256 amount ) public payable{
+        require(amount >0);
+        require(clients[idHashed].accountAddress != address(0));
+        clients[idHashed].accountAddress.
+        (amount);
+    }
+    function report(uint256 idHashed ,uint amount , uint256 time ) public  {
+        require(doctors[msg.sender]== true);
+        require(amount >0);
+        require(time < clients[idHashed].expirationDate );
+        require(time <= block.timestamp );
+        uint256 compensation = 0;
+        if (clients[idHashed].contractType == CONTRACT_TYPE.SILVER)
+            compensation = uint256(amount * 5 /10);
+        else if (clients[idHashed].contractType == CONTRACT_TYPE.GOLD)
+            compensation = uint256(amount * 3/4);
+        else if (clients[idHashed].contractType == CONTRACT_TYPE.PLATINIUM)
+            compensation = uint256(amount * 19/20);
+        clients[idHashed].inLottery = false;
+        refundClient(idHashed,compensation );
+
+    }
+
 }
